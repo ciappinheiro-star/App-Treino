6fc1d2ae6c1fdf5b57c20aa4c9cf3e0f0698d060
ciappinheiro-star <ciappinheiro@gmail.com>
2026-07-29 19:27:36 -0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'App Treino',
+  description: 'Aplicação de treinos',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    
+      {children}
+    
+  )
+}
